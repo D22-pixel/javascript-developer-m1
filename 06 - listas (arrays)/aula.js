40f8cd54f7d197c054dbe3fb39dce62d4eff6e4a
Denise Saldanha <79320578+D22-pixel@users.noreply.github.com>
2023-11-21 23:08:37 -0300

    Update aula.js

--- 06 - listas (arrays)/aula.js
+++ 06 - listas (arrays)/aula.js
@@ -1,5 +1,5 @@
 
-const notas = [];
+const notas = [lista];
 
 notas.push(10);
 notas.push(10);
@@ -14,3 +14,9 @@ for (let i = 0; i < notas.length; i++) {
 
 const media = soma / notas.length;
 console.log(media);
+
+anotações
+
+console.log(aluno.pop()); - tira o ultimo item da lista e coloca separado 
+                    shift - pega o primeiro numero da lista em separado
+length - mostra tamanho  da lista
